c02ea60ddae3299c6a072c9c7ceccba6f0ccbd15
Neil Patrick Angelo V. Falceso <nfalceso@RHEA.svi.com.ph>
2019-06-10 20:42:51 +0800

    wala

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'try-lang';
+  title = 'JSON to Table Example';
+  constructor (private httpService: HttpClient) { }
+  arrBirds: string [];
+
+  ngOnInit () {
+    this.httpService.get('./assets/birds.json').subscribe(
+      data => {
+        this.arrBirds = data as string [];	 // FILL THE ARRAY WITH DATA.
+        //  console.log(this.arrBirds[1]);
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    );
+  }
 }
---
+++ src/app/birds.ts
@@ -0,0 +1,5 @@
+export class PokemonList {
+    name: string;
+    id: number;
+    url: string;
+}
